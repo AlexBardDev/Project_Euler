10cbaee91b5a0587fabcb9ee0f81387943f57032
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-03-13 12:12:01 +0100

    Solution 15 added

---
+++ euler_15.py
@@ -0,0 +1,10 @@
+"""
+problem: https://projecteuler.net/problem=15
+"""
+
+#Roslve using the binomial law of probability for the Lattice Path
+
+import math
+n = 20
+
+print(int(math.factorial(2*n)/(math.factorial(n)**2)))
